c8f93180d2fed264cecacd047de34d3820b13db4
Service Web <sw@gssmail.com>
2024-03-18 11:09:13 -0500

    Commit changes in C:\Websites\Git\emarlow\6113\GCG_6113_Maintenance.sql

---
+++ GCG_6113_Maintenance.sql
@@ -0,0 +1,11 @@
+-- Database: GLOBALPLA Metadata version: 2 Exported: Sep 22, 2020
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6113_MAINTENANCE"(
+ "ZERO_BALANCE" BIT NOT NULL,
+ "AGE_BY_DUE_DATE" BIT NOT NULL,
+ "AGE_BY_INV_DATE" BIT NOT NULL );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
